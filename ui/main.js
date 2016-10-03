55a1973ae3afb3b034d4145c6d487cc15d3ed6a9
Pallab Ganguly <pallabkumarganguly@gmail.com>
2016-10-03 19:07:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,5 +5,6 @@ var element= document.getElementById("maintext");
 var wow= document.getElementById("dino");
 
 wow.onclick=function(){
-  wow.style.marginLeft="100px";  
+  //wow.style.marginLeft="100px";  
+  var interval = setInterval(moveLeft, 100);
 };
