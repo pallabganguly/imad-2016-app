c9af5ab618a468626f98ecdcc4d91ce77a518722
Pallab Ganguly <pallabkumarganguly@gmail.com>
2016-10-03 19:03:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,2 +1,9 @@
 console.log('Loaded!');
-var element= document.getElementById("maintext")
+
+var element= document.getElementById("maintext");
+
+var wow= document.getElementById("dino");
+
+wow.onclick=function(){
+  img.style.marginLeft="100px";  
+};
